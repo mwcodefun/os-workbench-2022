e16e8489251e312ff02f31336b6b417cb1048efb
tracer-nju <tracer@nju.edu.cn>
2022-07-25 22:54:47 +0800

    Linux ss-ub20 5.13.0-48-generic #54~20.04.1-Ubuntu SMP Thu Jun 2 23:37:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:54:47 up 42 days,  3:53,  1 user,  load average: 1.07, 1.11, 0.83

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -10,25 +10,25 @@ static void init() {
   h = info.height;
 }
 
-static void draw_tile(int x, int y, int w, int h, uint32_t color) {
-  uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
-  AM_GPU_FBDRAW_T event = {
-    .x = x, .y = y, .w = w, .h = h, .sync = 1,
-    .pixels = pixels,
-  };
-  for (int i = 0; i < w * h; i++) {
-    pixels[i] = color;
-  }
-  ioe_write(AM_GPU_FBDRAW, &event);
-}
+// static void draw_tile(int x, int y, int w, int h, uint32_t color) {
+//   uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
+//   AM_GPU_FBDRAW_T event = {
+//     .x = x, .y = y, .w = w, .h = h, .sync = 1,
+//     .pixels = pixels,
+//   };
+//   for (int i = 0; i < w * h; i++) {
+//     pixels[i] = color;
+//   }
+//   ioe_write(AM_GPU_FBDRAW, &event);
+// }
 
 void splash() {
   init();
-  for (int x = 0; x * SIDE <= w; x ++) {
-    for (int y = 0; y * SIDE <= h; y++) {
-      if ((x & 1) ^ (y & 1)) {
-        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
-      }
-    }
-  }
+  // for (int x = 0; x * SIDE <= w; x ++) {
+  //   for (int y = 0; y * SIDE <= h; y++) {
+  //     if ((x & 1) ^ (y & 1)) {
+  //       draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
+  //     }
+  //   }
+  // }
 }
